57e4cc067c1c0edfa028eddd8076fcefdf804862
Lucas Campos <lfgcampos@gmail.com>
2023-11-08 11:05:37 +0100

    Update build.gradle.kts

--- chat-getting-started-kotlin/build.gradle.kts
+++ chat-getting-started-kotlin/build.gradle.kts
@@ -6,7 +6,7 @@ val swaggerVersion: String by extra { "2.9.2" }
 plugins {
     id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
     kotlin("plugin.spring") version "1.9.20"
 }
 
