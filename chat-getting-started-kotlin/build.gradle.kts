9e71f9df8546e4bf8dc4b0fced63e1bc28fe1bfa
Lucas Campos <lfgcampos@gmail.com>
2025-03-25 11:03:36 +0100

    including plugin.spring

--- chat-getting-started-kotlin/build.gradle.kts
+++ chat-getting-started-kotlin/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
     id("org.springframework.boot") version "2.7.5"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "2.1.20"
-    kotlin("plugin.spring") version "2.1.10"
+    kotlin("plugin.spring") version "2.1.20"
 }
 
 group = "io.axoniq.labs.chat"
